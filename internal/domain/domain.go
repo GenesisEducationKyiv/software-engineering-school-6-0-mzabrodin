37b20d802150a751ceb3a04537f5dc92e8faabec
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2026-04-12 01:20:13 +0300

    add domain models for Repository and Subscription

---
+++ internal/domain/domain.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Repository struct {
+	ID          uuid.UUID
+	Name        string
+	LastSeenTag *string
+	CheckedAt   *time.Time
+	CreatedAt   time.Time
+}
+
+type Subscription struct {
+	ID               uuid.UUID
+	RepositoryID     uuid.UUID
+	Email            string
+	ConfirmToken     string
+	UnsubscribeToken string
+	Confirmed        bool
+	CreatedAt        time.Time
+}
