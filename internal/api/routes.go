9a54517fc89b86868b1762b194afbd6cfee402bc
Maksym Zabrodin <109875751+mzabrodin@users.noreply.github.com>
2026-05-30 00:01:59 +0300

    make logger injectable in API router and replace middleware with slog-based implementation

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -8,12 +9,12 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func NewRouter(h *Handler, apiKey string) http.Handler {
+func NewRouter(h *Handler, apiKey string, log *slog.Logger) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	r.Use(NewSlogMiddleware(log))
 	r.Use(middleware.Recoverer)
 	r.Use(MetricsMiddleware)
 
