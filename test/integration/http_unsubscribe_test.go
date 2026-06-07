524c52ad7030eccca0f13e0b99e6cc8afd038c97
Maksym Zabrodin <109875751+mzabrodin@users.noreply.github.com>
2026-06-08 01:39:11 +0300

    test: renamed http integration test as http_*.go

--- test/integration/http_unsubscribe_test.go
+++ test/integration/http_unsubscribe_test.go
@@ -10,21 +10,21 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type UnsubscribeSuite struct {
+type HTTPUnsubscribeSuite struct {
 	suite.Suite
 	srv *httptest.Server
 }
 
-func TestUnsubscribeSuite(t *testing.T) {
-	suite.Run(t, new(UnsubscribeSuite))
+func TestHTTPUnsubscribeSuite(t *testing.T) {
+	suite.Run(t, new(HTTPUnsubscribeSuite))
 }
 
-func (s *UnsubscribeSuite) SetupTest() {
+func (s *HTTPUnsubscribeSuite) SetupTest() {
 	truncateAll(s.T())
-	s.srv = newTestServer(s.T(), true)
+	s.srv = newTestHTTPServer(s.T(), true)
 }
 
-func (s *UnsubscribeSuite) TestSuccess() {
+func (s *HTTPUnsubscribeSuite) TestSuccess() {
 	_, unsubToken := subscribeAndGetTokens(s.T(), s.srv)
 
 	resp := doRequest(s.T(), http.MethodGet, s.srv.URL+"/api/unsubscribe/"+unsubToken, "", "")
@@ -37,17 +37,17 @@ func (s *UnsubscribeSuite) TestSuccess() {
 	s.Zero(count, "subscription should be deleted after /api/unsubscribe")
 }
 
-func (s *UnsubscribeSuite) TestInvalidTokenLength_Returns400() {
+func (s *HTTPUnsubscribeSuite) TestInvalidTokenLength_Returns400() {
 	resp := doRequest(s.T(), http.MethodGet, s.srv.URL+"/api/unsubscribe/tooshort", "", "")
 	s.Equal(http.StatusBadRequest, resp.StatusCode)
 }
 
-func (s *UnsubscribeSuite) TestUnknownToken_Returns404() {
+func (s *HTTPUnsubscribeSuite) TestUnknownToken_Returns404() {
 	resp := doRequest(s.T(), http.MethodGet, s.srv.URL+"/api/unsubscribe/"+randomHex64(), "", "")
 	s.Equal(http.StatusNotFound, resp.StatusCode)
 }
 
-func (s *UnsubscribeSuite) TestAlreadyUnsubscribed_Returns404() {
+func (s *HTTPUnsubscribeSuite) TestAlreadyUnsubscribed_Returns404() {
 	_, unsubToken := subscribeAndGetTokens(s.T(), s.srv)
 
 	resp := doRequest(s.T(), http.MethodGet, s.srv.URL+"/api/unsubscribe/"+unsubToken, "", "")
