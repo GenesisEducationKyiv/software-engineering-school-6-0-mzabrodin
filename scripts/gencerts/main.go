d5053797da837f8b0254f6e4a4c2586329d71ed6
Maksym Zabrodin <109875751+mzabrodin@users.noreply.github.com>
2026-06-08 09:23:45 +0300

    feat: add gencerts utility to generate TLS certificates

---
+++ scripts/gencerts/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github-release-notifier/internal/certgen"
+)
+
+func main() {
+	dir := "certs"
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	}
+
+	if err := certgen.Write(dir); err != nil {
+		log.Fatalf("gencerts: %v", err)
+	}
+
+	fmt.Printf("wrote ca.crt, server.crt/key and client.crt/key to %s\n", dir)
+}
