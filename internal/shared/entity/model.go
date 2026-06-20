ab788f7c0fdf5a1ee6156fde1575cd0cb74eee22
Maksym Zabrodin <109875751+mzabrodin@users.noreply.github.com>
2026-06-20 08:22:05 +0300

    refactor(subscription): change NewRepository and NewSubscription to return values instead of pointers

--- internal/shared/entity/model.go
+++ internal/shared/entity/model.go
@@ -7,18 +7,15 @@ import (
 )
 
 type Repository struct {
-	ID          uuid.UUID
-	Name        string
-	LastSeenTag *string
-	CheckedAt   *time.Time
-	CreatedAt   time.Time
+	ID        uuid.UUID
+	Name      string
+	CreatedAt time.Time
 }
 
 type Subscription struct {
 	ID               uuid.UUID
 	RepositoryID     uuid.UUID
 	Email            string
-	ConfirmToken     string
 	UnsubscribeToken string
 	Confirmed        bool
 	CreatedAt        time.Time
